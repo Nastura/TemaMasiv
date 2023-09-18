61ee7b642dcf8c1f053bc0f3c3c183eca2f250d0
Nastura <chulmost@gmail.com>
2023-09-18 16:28:14 +0300

    Changing Variable and Method Names

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -4,82 +4,82 @@ public class StatsService {
 
     // МЕТОД 1. Сумма всех продаж;
 
-    public int sumSale(long[] sales) {
-        long sumArray = 0; //  изначальная сумма = 0
+    public int salesAmount(long[] sales) {
+        long initialSalesAmount = 0; //  изначальная сумма = 0
 
         for (long value : sales) {
-            sumArray += value;
+            initialSalesAmount += value;
         }
-        return (int) sumArray;
+        return (int) initialSalesAmount;
     }
 
     // МЕТОД 2. Cредняя сумма продаж в месяц;
     // среднее = сумма чисел/количество чисел.
-    public int AverageSumSales(long[] sales) {
-        int AverageSum = 0;
+    public int averageSalesAmount(long[] sales) {
+        int initialSalesAmount = 0;
         for (long d : sales) {
-            AverageSum += d;
+            initialSalesAmount += d;
         }
 
-        return AverageSum / sales.length;
+        return initialSalesAmount / sales.length;
 
     }
 
     // МЕТОД 3. Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму*;;
 
-    public int maxSales(long[] sales) {
-        int maxMonth = 0; // номер месяца с максимальными продажами среди просмотренных ранее
+    public int monthsWithMaximumSales(long[] sales) {
+        int monthsMaximumSales = 0; // номер месяца с максимальными продажами среди просмотренных ранее
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] >= sales[maxMonth]) { // значит, в рассматриваемом i-м месяце продаж больше
-                maxMonth = i; // запомним его как максимальный
+            if (sales[i] >= sales[monthsMaximumSales]) { // значит, в рассматриваемом i-м месяце продаж больше
+                monthsMaximumSales = i; // запомним его как максимальный
             }
         }
 
-        return maxMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return monthsMaximumSales + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
     }
 
     // МЕТОД 4. Номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму*;
 
-    public int minSales(long[] sales) {
-        int minMonth = 0; // номер месяца с минимальными продажами среди просмотренных ранее
+    public int monthsWithMinimalSales(long[] sales) {
+        int monthsMinimumSales = 0; // номер месяца с минимальными продажами среди просмотренных ранее
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] <= sales[minMonth]) { // значит, в рассматриваемом i-м месяце продаж меньше
-                minMonth = i; // запомним его как минимальный
+            if (sales[i] <= sales[monthsMinimumSales]) { // значит, в рассматриваемом i-м месяце продаж меньше
+                monthsMinimumSales = i; // запомним его как минимальный
             }
         }
 
-        return minMonth + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
+        return monthsMinimumSales + 1; // месяца нумеруются с 1, а индексы массива с 0, нужно сдвинуть ответ на 1
     }
 
     // МЕТОД 5. Количество месяцев, в которых продажи были ниже среднего (см. п.2);
 
-    public int NumberOfMonthsMinSale(long[] sales) {
-        int SredMonth = AverageSumSales(sales);
-        int SummaMes = 0;
+    public int salesBelowAverage(long[] sales) {
+        int averageSales = averageSalesAmount(sales);
+        int initialSalesAmount = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < SredMonth)
-                SummaMes++;
+            if (sales[i] < averageSales)
+                initialSalesAmount++;
         }
 
-        return SummaMes;
+        return initialSalesAmount;
 
     }
 
     // МЕТОД 6. Количество месяцев, в которых продажи были выше среднего (см. п.2);
 
-    public int NumberOfMonthsMaxSale(long[] sales) {
-        int SredMonth = AverageSumSales(sales);
-        int maxMonth = 0;
+    public int salesAboveAverage(long[] sales) {
+        int averageSales = averageSalesAmount(sales);
+        int initialSalesAmount = 0;
 
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] > SredMonth) {
-                maxMonth++;
+            if (sales[i] > averageSales) {
+                initialSalesAmount++;
             }
         }
-        return maxMonth;
+        return initialSalesAmount;
     }
 }
 
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -5,7 +5,7 @@ import ru.netology.stats.StatsService;
 public class StatsServiceTest {
 // Сумма 180 Кол-во чисел 12
 
-    @Test    //Test 1
+    @Test    //Test 1 Сумма всех продаж;
 
     public void test1() {
 
@@ -13,7 +13,7 @@ public class StatsServiceTest {
 
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 180;
-        int actualDay = service.sumSale(sales);
+        int actualDay = service.salesAmount(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
@@ -25,54 +25,55 @@ public class StatsServiceTest {
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 15;
-        int actualDay = service.AverageSumSales(sales);
+        int actualDay = service.averageSalesAmount(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
 
-    @Test    //Test 3
+    @Test    //Test 3 Номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму*;;
 
     public void test3() {
 
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 8;
-        int actualDay = service.maxSales(sales);
+        int actualDay = service.monthsWithMaximumSales(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
 
-    @Test    //Test 4
+    @Test    //Test 4 Номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму*;
 
     public void test4() {
 
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 9;
-        int actualDay = service.minSales(sales);
+        int actualDay = service.monthsWithMinimalSales(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
 
-    @Test    //Test 5
+    @Test    //Test 5 Количество месяцев, в которых продажи были ниже среднего (см. п.2);
 
     public void test5() {
 
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 5;
-        int actualDay = service.NumberOfMonthsMinSale(sales);
+        int actualDay = service.salesBelowAverage(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
 
-    @Test    //Test 6
+    @Test    //Test 6 Количество месяцев, в которых продажи были выше среднего (см. п.2);
+
     public void test6() {
 
         StatsService service = new StatsService();
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedDay = 5;
-        int actualDay = service.NumberOfMonthsMaxSale(sales);
+        int actualDay = service.salesAboveAverage(sales);
         Assertions.assertEquals(expectedDay, actualDay);
 
     }
